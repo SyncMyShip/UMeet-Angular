030569b102796999d30d40e582545f54644fb1f2
Simba <jfrancis1195@gmail.com>
2024-10-13 01:37:17 -0400

    setup user login and signup components

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,10 +5,147 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://reelrendezvous-0ea25cfde7d6.herokuapp.com';
 @Injectable({
   providedIn: 'root'
 })
+
+
+export class FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+ // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {
+  }
+
+// User Registration //
+ // Making the api call for the user registration endpoint
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + '/signup', userDetails).pipe(
+    // catchError(this.handleError)
+    );
+  }
+
+
+// User Login //
+  public userLogin(userDetails: any): Observable<any> {
+    return this.http.post(apiUrl + '/login', userDetails).pipe(
+    // catchError(this.handleError)
+    );
+  }
+
+
+// Get All Movies //
+  public getAllMovies(): Observable<any> {
+    return this.http.get(apiUrl + '/movies', {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+// Get One Movie //
+  public getOneMovie(title: string): Observable<any> {
+    return this.http.get(apiUrl + '/movies/' + title, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+// Get Movie By Director //
+  public getMovieByDirector(directorName: string): Observable<any> {
+    return this.http.get(apiUrl + '/movies/directors/' + directorName, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+// Get Movie By Genre //
+  public getMovieByGenre(genreName: string): Observable<any> {
+    return this.http.get(apiUrl + '/movies/genres/' + genreName, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+
+// Get User //
+  public getUser(username: string): Observable<any> {
+    return this.http.get(apiUrl + '/users/' + username, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+
+// Get User Favorites //
+  public getUserFavorites(username: string, title: string): Observable<any> {
+    return this.http.get(apiUrl + '/users/' + username + 'movies/' + title, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+
+// Add User Favorites //
+  public addUserFavorites(username: string, title: string): Observable<any> {
+    return this.http.post(apiUrl + '/users/' + username + 'movies/' + title, {
+    // catchError(this.handleError)
+    })
+  }
+
+
+
+// Edit User //
+  public editUser(userData: any, username: string): Observable<any> {
+    return this.http.put(apiUrl + '/users/' + username, userData, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+// Delete User //
+  public deleteUser(username: string): Observable<any> {
+    return this.http.delete(apiUrl + '/users/' + username, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+
+
+// Delete User Favorite //
+  public deleteUserFavorite(username: string, title: string): Observable<any> {
+    return this.http.delete(apiUrl + '/users/' + username + 'movies/' + title, {
+      headers: new HttpHeaders({
+        Authorization: '<Bearer Token>'
+      })
+    // catchError(this.handleError)
+    })
+  }
+}
+
 
 // private handleError(error: HttpErrorResponse): any {
 //   if (error.error instanceof ErrorEvent) {
@@ -21,180 +158,3 @@ const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
 //   return throwError(
 //   'Something bad happened; please try again later.');
 // }
-
-// User Registration //
-export class UserRegistrationService {
-  // Inject the HttpClient module to the constructor params
-  // This will provide HttpClient to the entire class, making it available via this.http
-  constructor(private http: HttpClient) {
-  }
- // Making the api call for the user registration endpoint
-  public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
-    return this.http.post(apiUrl + '/signup', userDetails).pipe(
-    // catchError(this.handleError)
-    );
-  }
-}
-// 
-
-
-// User Login //
-export class UserLogin {
-  constructor(private http: HttpClient) {
-  }
-  public userLogin(userDetails: any): Observable<any> {
-    return this.http.post(apiUrl + '/login', userDetails).pipe(
-    // catchError(this.handleError)
-    );
-  }
-}
-
-
-// Get All Movies //
-export class GetAllMovies {
-  constructor(private http: HttpClient) {
-  }
-  public getAllMovies(): Observable<any> {
-    return this.http.get(apiUrl + '/movies', {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Get One Movie //
-export class GetOneMovie {
-  constructor(private http: HttpClient) {
-  }
-  public getOneMovie(title: string): Observable<any> {
-    return this.http.get(apiUrl + '/movies/' + title, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Get Movie By Director //
-export class GetMovieByDirector {
-  constructor(private http: HttpClient) {
-  }
-  public getMovieByDirector(directorName: string): Observable<any> {
-    return this.http.get(apiUrl + '/movies/directors/' + directorName, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Get Movie By Genre //
-export class GetMovieByGenre {
-  constructor(private http: HttpClient) {
-  }
-  public getMovieByGenre(genreName: string): Observable<any> {
-    return this.http.get(apiUrl + '/movies/genres/' + genreName, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-
-// Get User //
-export class GetUser{
-  constructor(private http: HttpClient) {
-  }
-  public getUser(username: string): Observable<any> {
-    return this.http.get(apiUrl + '/users/' + username, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Get User Favorites //
-export class GetUserFavorites{
-  constructor(private http: HttpClient) {
-  }
-  public getUserFavorites(username: string, title: string): Observable<any> {
-    return this.http.get(apiUrl + '/users/' + username + 'movies/' + title, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-
-// Add User Favorites //
-export class AddUserFavorites{
-  constructor(private http: HttpClient) {
-  }
-  public addUserFavorites(username: string, title: string): Observable<any> {
-    return this.http.post(apiUrl + '/users/' + username + 'movies/' + title, {
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Edit User //
-export class EditUser{
-  constructor(private http: HttpClient) {
-  }
-  public editUser(userData: any, username: string): Observable<any> {
-    return this.http.put(apiUrl + '/users/' + username, userData, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Delete User //
-export class DeleteUser{
-  constructor(private http: HttpClient) {
-  }
-  public deleteUser(username: string): Observable<any> {
-    return this.http.delete(apiUrl + '/users/' + username, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
-
-
-// Delete User Favorite //
-export class DeleteUserFavorite{
-  constructor(private http: HttpClient) {
-  }
-  public deleteUserFavorite(username: string, title: string): Observable<any> {
-    return this.http.delete(apiUrl + '/users/' + username + 'movies/' + title, {
-      headers: new HttpHeaders({
-        Authorization: '<Bearer Token>'
-      })
-    // catchError(this.handleError)
-    })
-  }
-}
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,62 @@
+// src/app/user-registration-form/user-login--form.component.ts
+import { Component, OnInit, Input } from '@angular/core';
+// You'll use this import to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// This import brings in the API calls we created in 6.2
+import { FetchApiDataService } from '../fetch-api-data.service';
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+      public fetchApiData: FetchApiDataService,
+      public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+      public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+  }
+
+// This is the function responsible for sending the form inputs to the backend
+  // registerUser(): void {
+  //   this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+  //       // Logic for a successful user registration goes here! (To be implemented)
+  //       this.dialogRef.close(); // This will close the modal on success!
+  //       this.snackBar.open("User create success", 'OK', {
+  //           duration: 2000
+  //       });
+  //   }, (result) => {
+  //       this.snackBar.open("User create fail", 'OK', {
+  //           duration: 2000
+  //       });
+  //   });
+  // }
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe({
+      next: (result) => {
+        // Logic for a successful user registration goes here! (To be implemented)
+        this.dialogRef.close(); // This will close the modal on success!
+        this.snackBar.open(result, 'OK', {
+          duration: 2000
+        });
+      },
+      error: (error) => {
+        this.snackBar.open('Login failed', 'OK', {
+          duration: 2000
+        });
+      },
+      // complete: () => {
+      //   // Handle completion cases
+      // }
+    });
+  }
+}
